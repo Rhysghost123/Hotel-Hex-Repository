d71b8f87d9f7897f60613dac47cf0841f2c6a293
Rhysghost123 <146122755+Rhysghost123@users.noreply.github.com>
2025-07-16 01:12:00 -0400

    added dash

--- Hotel Hex/Assets/Scripts/PlayerController2D.cs
+++ Hotel Hex/Assets/Scripts/PlayerController2D.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerController2D : MonoBehaviour
 {
@@ -19,6 +20,12 @@ public class PlayerController2D : MonoBehaviour
     public float jumpVelocity = 5f;
     public float maxFallSpeed = -15f;
 
+    //variables needed for the dash
+    public float dashStrength = 40f;
+    float normalGravity;
+    bool isDashing;
+    bool canDash;
+
     Animator anim;
 
 
@@ -29,6 +36,7 @@ public class PlayerController2D : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        normalGravity = rb.gravityScale;
     }
 
     // Update is called once per frame
@@ -37,6 +45,12 @@ public class PlayerController2D : MonoBehaviour
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float xVelocity = horizontalInput * moveSpeed;
         float yVelocity = rb.velocity.y;
+        UnityEngine.Vector2 dir = new UnityEngine.Vector2(horizontalInput, Input.GetAxisRaw("Vertical"));
+
+        if (IsGrounded())
+        {
+            canDash = true;
+        }
 
         if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
@@ -72,13 +86,42 @@ public class PlayerController2D : MonoBehaviour
             transform.localScale = new UnityEngine.Vector3(-Math.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
 
-        rb.velocity = new UnityEngine.Vector2(xVelocity, yVelocity);
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            if (dir == new UnityEngine.Vector2(0, 0))
+            {
+                dir = new UnityEngine.Vector2(transform.localScale.x, Input.GetAxisRaw("Vertical"));
+                StartCoroutine(Dash(dir, dashStrength));
+            }
+            else
+            {
+                StartCoroutine(Dash(dir, dashStrength));
+                canDash = false;
+            }
+            
+        }
+        else if (!isDashing)
+        {
+            rb.velocity = new UnityEngine.Vector2(xVelocity, yVelocity);
+        }
 
     }
 
     bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundChecker.position, groundCheckerRadius, ground);
+    }
+
+    IEnumerator Dash(UnityEngine.Vector2 direction, float magnitude)
+    {
+        isDashing = true;
+        rb.gravityScale = 0;
+        UnityEngine.Vector2 originalVelocity = rb.velocity;
+        rb.velocity = direction * magnitude;
+        yield return new WaitForSeconds(.2f);
+        rb.velocity = direction * magnitude/2;
+        rb.gravityScale = normalGravity;
+        isDashing = false;
     }
 
 
