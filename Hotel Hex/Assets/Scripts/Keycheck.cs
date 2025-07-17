5af0a4def6474632d1425ceb37d69e49fc129e38
insertNamehere123456 <ottoknight0603@gmail.com>
2025-07-16 22:36:06 -0400

    s

--- Hotel Hex/Assets/Scripts/Keycheck.cs
+++ Hotel Hex/Assets/Scripts/Keycheck.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Key : MonoBehaviour
+public class Keycheck : MonoBehaviour
 {
+    public int key_left;
+    public bool doorlocked = true;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
-    public void OnTriggerEnter2D(Collider2D player)
+    void Update()
     {
-        GameObject.Destroy(gameObject);
+        
     }
 }
-
